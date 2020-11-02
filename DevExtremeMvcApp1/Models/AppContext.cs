d67cd0316111d7fc6dd24916a384ef8dd574d591
FrancisMbatia <https://github.com/pwambui2020/pizza-palace.git>
2020-11-02 12:24:13 +0300

    Add project files.

---
+++ DevExtremeMvcApp1/Models/AppContext.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace DevExtremeMvcApp1.Models
+{
+    public class AppContext : DbContext
+    {
+        public DbSet<County> Counties { get; set; }
+        public DbSet<Owner> Owners { get; set; }
+    }
+}
---
+++ DevExtremeMvcApp1/Models/Owner.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace DevExtremeMvcApp1.Models
+{
+    public class Owner
+    {
+        [Key]
+        public int Id { get; set; }
+        public string FullName { get; set; }
+        public string PhoneNumber1 { get; set; }
+        public string PhoneNumber2 { get; set; }
+        public string Email { get; set; }
+        public string Address { get; set; }
+        public string Town { get; set; }
+        public string County { get; set; }
+        public string Gender { get; set; }
+        public string CreatedBy { get; set; }
+        public DateTime CreatedOn { get; set; }
+        public string ModifiedBy { get; set; }
+        public DateTime ModifiedOn { get; set; }
+        public bool IsDeleted { get; set; }
+        public string DeletedBy { get; set; }
+        public DateTime DeletedOn { get; set; }
+    }
+}
